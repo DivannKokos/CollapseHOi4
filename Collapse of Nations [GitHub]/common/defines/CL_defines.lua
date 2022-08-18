c4dfd8bcfb14aa8fd37abc11b1544bc9b845694e
DivannKokos <divann.2@yandex.ru>
2022-08-18 10:16:51 +0700

    Файл и папка теперь имеют одно название
    Файл и папка теперь имеют одно название, Collapse of Nations [GitHub]

---
+++ Collapse of Nations [GitHub]/common/defines/CL_defines.lua
@@ -0,0 +1,8 @@
+NDefines.NGame.START_DATE = "2023.1.1.12"
+NDefines.NGame.END_DATE = "2040.1.1.1"
+
+--взято из мода SOO optimization вроде как оптимизирует игру, я не ебу как, но наверное работает я хз
+NDefines.NGame.COMBAT_LOG_MAX_MONTHS = 6
+NDefines.NGame.LAG_DAYS_FOR_LOWER_SPEED = 60
+NDefines.NGame.LAG_DAYS_FOR_PAUSE = 100
+NDefines.NGame.GAME_SPEED_SECONDS = { 0.4, 0.25, 0.1, 0.05, 0.0 }
